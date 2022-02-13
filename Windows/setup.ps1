831d25bd73d724e06c5d85eb4cf7cdbccd09fbeb
Amarnath Jamale <amarnath.jamale@outlook.com>
2022-02-13 12:46:05 -0800

    Script path defined

---
+++ Windows/setup.ps1
@@ -0,0 +1,13 @@
+# Debloat Windows
+
+# Setup Windows Variables
+
+# Modify Windows Configurations
+
+# Install Necessary Drivers
+
+# Install Core Apps
+
+# Install Necessary Applications with their customizations
+
+# Reboot
