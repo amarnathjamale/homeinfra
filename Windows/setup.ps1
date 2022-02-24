42d535b68e53062330589bb27a596a530acf0705
Amarnath Jamale <Amarnath.Jamale@outlook.com>
2022-02-25 01:27:34 +0530

    Windows Debloater added

--- Windows/setup.ps1
+++ Windows/setup.ps1
@@ -1,4 +1,9 @@
 # Debloat Windows
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+Set-Location $DownloadsFolder
+Invoke-RestMethod script.sophi.app | Invoke-Expression
+Set-Location Sophia*
+.\Sophia.ps1
 
 # Setup Windows Variables
 
@@ -7,6 +12,10 @@
 # Install Necessary Drivers
 
 # Install Core Apps
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+winget install Git.Git
+winget install python
 
 # Install Necessary Applications with their customizations
 
