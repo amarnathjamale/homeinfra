94221e45a8ea66ca507554f8cf5df99612ccebeb
Amarnath Jamale <Amarnath.Jamale@outlook.com>
2022-12-13 21:33:11 +0100

    Updated Windows setup script

--- Windows/setup.ps1
+++ Windows/setup.ps1
@@ -1,62 +1,62 @@
-# Debloat Windows
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-Invoke-RestMethod script.sophi.app | Invoke-Expression
-Set-Location $DownloadsFolder\Sophia*
-.\Sophia.ps1
+function VSCode {
+    winget install -e --id Microsoft.VisualStudioCode
+}
 
-# Setup for Ansible Host
-# Upgrading PowerShell and .NET Framework
-$url = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Upgrade-PowerShell.ps1"
-$file = "$env:temp\Upgrade-PowerShell.ps1"
-$username = "Administrator"
-$password = "Password"
+function WSL {
+    wsl --install
+    winget install -e --id whitewaterfoundry.fedora-remix-for-wsl
+}
 
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+function Hardening {
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+    Invoke-RestMethod script.sophi.app | Invoke-Expression
+    Set-Location $DownloadsFolder\Sophia*
+    .\Sophia.ps1
+}
 
-# version can be 3.0, 4.0 or 5.1
-&$file -Version 5.1 -Username $username -Password $password -Verbose
+function Apps {
+    winget install Git.Git
+    winget install python
+    choco install mpv -y
+}
 
-# WinRM Memory Hotfix
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url_hostfix = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Install-WMF3Hotfix.ps1"
-$file_hostfix = "$env:temp\Install-WMF3Hotfix.ps1"
+function ALL {
+    Apps
+    VSCode
+    WSL
+    Hardening
+}
 
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url_hostfix, $file_hostfix)
-powershell.exe -ExecutionPolicy ByPass -File $file_hostfix -Verbose
+function Show-Menu {
+    param (
+        [string]$Title = "Amar's Windows Setup"
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
 
-# This isn't needed but is a good security practice to complete
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+    Write-Host "1: VSCode Setup"
+    Write-Host "2: WSL Setup"
+    Write-Host "3: Hardening"
+    Write-Host "4: Deafult Apps"
+    Write-Host "5. ALL"
+    Write-Host "q: Quit"
 
-$reg_winlogon_path = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
-Set-ItemProperty -Path $reg_winlogon_path -Name AutoAdminLogon -Value 0
-Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultUserName -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultPassword -ErrorAction SilentlyContinue
+}
 
-# winrm setup 
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url_winrm = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
-$file_winrm = "$env:temp\ConfigureRemotingForAnsible.ps1"
-
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url_winrm, $file_winrm)
-
-powershell.exe -ExecutionPolicy ByPass -File $file_winrm
+Show-Menu -Title "Amar's Windows Setup"
+$selection = Read-Host "Please make a selection"
 
 
-choco install --package-parameters=/SSHServerFeature openssh
-
-# Modify Windows Configurations
-
-# Install Necessary Drivers
-
-# Install Core Apps
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-winget install Git.Git
-winget install python
-choco install mpv -y
-
-# Install Necessary Applications with their customizations
-
-# Reboot
+do {
+    Show-Menu
+    $selection = Read-Host "Please make a selection"
+    switch ($selection) {
+        '1' { VSCode }
+        '2' { Hardening }
+        '3' { WSL }
+        '4' { Apps }
+        '5' { ALL }
+    }
+    pause
+}
+until ($selection -eq 'q')
